a9269bea4c723df5bd85bff6298f9abcb8848d02
Hyungsuk Kang <hyungsukkang@Hyungsuks-MacBook-Pro.local>
2016-11-22 17:26:14 +0900

    calculator app

--- Final/ViewController.swift
+++ Final/ViewController.swift
@@ -7,13 +7,52 @@
 //
 
 import UIKit
+import AudioKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var amplitudeLabel: UILabel!
+    
+    @IBOutlet weak var audioInputPlot: EZAudioPlot!
+    
+    var mic: AKMicrophone!
+    var tracker: AKFrequencyTracker!
+    var silence: AKBooster!
+    
+    
+    func setupPlot() {
+        let plot = AKNodeOutputPlot(mic, frame: audioInputPlot.bounds)
+        plot.plotType = .rolling
+        plot.shouldFill = true
+        plot.shouldMirror = true
+        plot.color = UIColor.blue
+        audioInputPlot.addSubview(plot)
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        AKSettings.audioInputEnabled = true
+        mic = AKMicrophone()
+        tracker = AKFrequencyTracker.init(mic)
+        silence = AKBooster(tracker, gain: 0)
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        AudioKit.output = silence
+        AudioKit.start()
+        setupPlot()
+        Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.updateUI), userInfo: nil, repeats: true)
+    }
+
+    func updateUI() {
+            amplitudeLabel.text = String(format: "%0.2f", tracker.amplitude)
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
